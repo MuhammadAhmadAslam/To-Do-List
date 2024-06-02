2676d6bcc0fb7241435a88ba6f779a6d959d8526
MuhammadAhmadAslam <prince1ahmed111@gmail.com>
2024-06-02 21:08:14 +0500

    Commit

--- todo.js
+++ todo.js
@@ -1,21 +1,55 @@
-    let btn = document.getElementById('btn')
+  let body = document.getElementsByTagName('body')
+  body[0].style.backgroundColor = '#631A86'
+  let btn = document.getElementById('btn')
     let i = 1
     btn.addEventListener('click', () => {
         
         let rowDiv = document.getElementById('row')
     var inputField = document.getElementById('inputField')
-    let editBtn = document.createElement('button')
-    editBtn.setAttribute('class' , 'button')
-    editBtn.innerHTML = `<i class="fa-solid fa-pen-to-square"></i>`
-    let newDiv = document.createElement('div')
-        newDiv.className = 'col-sm-12 col-lg-3 col-md-4 newDiv'
-        newDiv.innerHTML = `${i} <br> ${inputField.value} <br> <span id='buttonwalaSpan'></span>`
-        newDiv.appendChild(editBtn)
-        rowDiv.appendChild(newDiv)
-    inputField.value = ''
-    i++
-    console.log(editBtn);
-    editBtn.addEventListener('click', () => {
-           newDiv.remove()
+    if (inputField.value != '') {
+        let editBtn = document.createElement('button')
+        let deleteButton = document.createElement('button')
+        editBtn.setAttribute('class' , 'button1')
+        deleteButton.setAttribute('class' , 'button')
+        editBtn.innerHTML = `<i class="fa-solid fa-pen-to-square"></i>`
+        deleteButton.innerHTML = `<i class="fa-solid fa-trash"></i>`
+        let newDiv = document.createElement('div')
+            newDiv.className = 'col-sm-6 col-lg-3 col-md-4 newDiv'
+            // newDiv.innerHTML = `<span class='textSpan'>${inputField}</span> <br>`
+        let textSpan = document.createElement('div')
+        textSpan.innerText = inputField.value
+        textSpan.setAttribute('class', 'textSpan')
+            newDiv.appendChild(textSpan)
+            newDiv.appendChild(editBtn)
+            newDiv.appendChild(deleteButton)
+            rowDiv.appendChild(newDiv)
+        inputField.value = ''
+        i++
+        console.log(editBtn);
+        editBtn.addEventListener('click', () => {
+            let newInput = document.createElement('input')
+            newDiv.appendChild(newInput)
+            newDiv.removeChild(editBtn)
+            newDiv.removeChild(deleteButton)
+            let saveBtn = document.createElement('button')
+            newDiv.appendChild(saveBtn)
+            saveBtn.innerText = 'Save'
+            saveBtn.setAttribute('class', 'saveBtn')
+
+            saveBtn.addEventListener('click', () => {
+                newDiv.innerText = newInput.value;
+                newDiv.appendChild(editBtn)
+                newDiv.appendChild(deleteButton)
+                console.log(true);
+            })
         })
+        
+        
+        deleteButton.addEventListener('click', () => {
+            newDiv.remove()
+        })    
+
+        }else{
+            alert('Write a TO DO in input field')
+        }
     })
